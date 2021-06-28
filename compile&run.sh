b32fcbd41df3dcc31c585444d5a0a8b2009948e4
fwestphal <fabian.westphal@student.uni-luebeck.de>
2021-06-28 23:48:50 +0200

    compile script fixed & optimization flags added (shouldn't change anything)

--- compile&run.sh
+++ compile&run.sh
@@ -1,11 +1,13 @@
-if [ "$#" != "0" ] 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./isa-l_crypto/bin
+export PATH=$PATH:/usr/local/cuda/bin
+if [ "$#" != "0" ]
 then
     echo "Compile Test:"
-    nvcc -o test test_bits_aes.cu -I CUDA-uint128 -I isa-l_crypto/include -L isa-l_crypto/bin -lisal_crypto
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./isa-l_crypto/bin
+    nvcc -lineinfo -G -o test test_bits_aes.cu -I CUDA-uint128 -I isa-l_crypto/include -L isa-l_crypto/bin -lisal_crypto
     ./test
 else
     echo "Compile AES:"
-    nvcc -o aes bits_aes.cu -I CUDA-uint128 -I isa-l_crypto/include -L isa-l_crypto/bin -lisal_crypto
+    nvcc  --maxrregcount=900 -O3 -Xptxas -O3,-v -Xcompiler -O3 -use_fast_math -o aes bits_aes.cu -I CUDA-uint128 -I isa-l_crypto/include  -L isa-l_crypto/bin -lisal_crypto
     ./aes
 fi
+
